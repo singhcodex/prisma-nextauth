544d8bb22f59cffabd39d572e2cedcc639263f8a
singhcodex <singh.hessen@gmail.com>
2023-04-16 12:45:00 +0200

    prisma and nextauth app done

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,32 @@
+import prisma from "@/lib/prisma";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import NextAuth from "next-auth/next";
+import EmailProvider from "next-auth/providers/email";
+
+export default NextAuth({
+    providers:[
+        EmailProvider({
+            server: process.env.EMAIL_SERVER,
+            from: process.env.EMAIL_FROM
+        })
+    ],
+
+    database: process.env.DATABASE_URL,
+    secret: process.env.SECRET,
+
+    session: {
+        jwt: true,
+        maxAge: 30 * 24* 60 * 60 //30 days
+    },
+
+    adapter: PrismaAdapter(prisma),
+
+    callbacks: {
+        session: async ({session, user}) => {
+            session.user.id = user.id
+            session.user.username = user.username
+            return Promise.resolve(session)
+        },
+    },
+
+})
